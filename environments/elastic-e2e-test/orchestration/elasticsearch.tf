27454e6844bef30ee915354a952c949b44a75c31
Pedro Osório <pedroosorio.eeic@gmail.com>
2022-10-06 11:03:04 +0100

    ST-1355: Added elastic-e2e-test environment

---
+++ environments/elastic-e2e-test/orchestration/elasticsearch.tf
@@ -0,0 +1,27 @@
+module "elasticsearch" {
+  source   = "../../../ska-ser-orchestration/openstack-elasticsearch-cluster"
+  defaults = var.defaults
+  providers = {
+    openstack = openstack
+  }
+
+  # TODO: Set to 1 master and 1 data when playbook supports it
+
+  elasticsearch = {
+    name = "elasticsearch-e2e-test"
+    master = {
+      size               = 2
+      data_volume_size   = 10
+      docker_volume_size = 10
+    }
+    data = {
+      size               = 0
+      data_volume_size   = 10
+      docker_volume_size = 10
+    }
+    kibana = {
+      size               = 0
+      docker_volume_size = 10
+    }
+  }
+}
