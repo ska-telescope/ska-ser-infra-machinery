b3e3f86c53be9ab25e435fbe4c1b5b97968c2637
Miguel Santos <miguel.santos@criticalsoftware.com>
2022-10-24 19:06:51 +0100

    BANG-29: Add engage enviroment files

---
+++ environments/engage/orchestration/ceph.tf
@@ -0,0 +1,21 @@
+module "ceph_cluster" {
+  source   = "../../../ska-ser-orchestration/openstack-ceph-cluster"
+  defaults = var.defaults
+  providers = {
+    openstack = openstack
+  }
+
+  ceph = {
+    name   = "ceph"
+    master = {
+      size = 3
+      data_volume_size = 10,
+      wal_volume_size = 10,
+      flavor = "c3.large"
+      image = "Ubuntu-20.04"  
+    }
+    worker = {
+      size = 0
+    }
+  }
+}
