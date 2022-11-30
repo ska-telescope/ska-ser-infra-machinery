db9d587b5a90b859937f36c2b1c8f3e93fd612f7
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2022-11-30 10:05:52 +0000

    ST-1442 update backend
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- datacentres/stfc-techops/e2e/orchestration/monitoring/terraform.tf
+++ datacentres/stfc-techops/e2e/orchestration/monitoring/terraform.tf
@@ -9,20 +9,8 @@ terraform {
     }
   }
 
-  # Requires the following environment variables:
-  # export ENVIRONMENT="<environment name>"
-  # export GITLAB_PROJECT_ID="<gitlab project id>"
-  # export TF_HTTP_USERNAME="<gitlab username>"
-  # export TF_HTTP_PASSWORD="<gitlab user access token>"
-  # export TF_HTTP_ADDRESS="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/terraform/state/${ENVIRONMENT}-terraform-state"
-  # export TF_HTTP_LOCK_ADDRESS="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/terraform/state/${ENVIRONMENT}-terraform-state/lock"
-  # export TF_HTTP_UNLOCK_ADDRESS="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/terraform/state/${ENVIRONMENT}-terraform-state/lock"
-
-  backend "http" {
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = 5
-    retry_wait_max = 30
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 # --------------------------------------------------- #
