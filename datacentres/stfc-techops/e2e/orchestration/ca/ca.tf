a195e5f59b771c81b8a82b1810b7a4bd51c16b52
sawdyj <james.sawdy@cgi.com>
2023-01-05 12:38:40 +0000

    ST-1433: Added e2e CA environment

---
+++ datacentres/stfc-techops/e2e/orchestration/ca/ca.tf
@@ -0,0 +1,12 @@
+module "ca" {
+  source   = "../../../../../ska-ser-orchestration/openstack-instance"
+  defaults = var.defaults
+  providers = {
+    openstack = openstack
+  }
+
+  configuration = {
+    name         = join("-", [var.datacentre, var.environment, var.service, var.ci_pipeline_id])
+    flavor       = "l3.nano"
+  }
+}
---
+++ datacentres/stfc-techops/e2e/orchestration/ca/variables.tf
@@ -0,0 +1,40 @@
+variable "datacentre" {
+  type        = string
+  description = "The datacentre name"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment name"
+}
+
+variable "service" {
+  type        = string
+  description = "The orchestration service name"
+}
+
+variable "ci_pipeline_id" {
+  type        = string
+  description = "The id of the ci pipeline"
+}
+
+variable "openstack" {
+  type = object({
+    cloud      = string
+    project_id = string
+  })
+}
+
+variable "defaults" {
+  type = object({
+    availability_zone   = string
+    flavor              = string
+    image               = string
+    keypair             = string
+    network             = string
+    jump_host           = optional(string)
+    floating_ip_network = optional(string)
+    vpn_cidr_blocks     = optional(list(string))
+  })
+  description = "Set of default values used when creating OpenStack instances"
+}
