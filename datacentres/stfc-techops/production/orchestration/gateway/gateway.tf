a7647847c611c19ae0341543ff4b2351a6ce2d0a
Bruno_Ribeiro <bruno.engelec@gmail.com>
2023-01-09 16:51:32 +0000

    BANG-49: add gateway machine to stfc etchops production

---
+++ datacentres/stfc-techops/production/orchestration/gateway/gateway.tf
@@ -0,0 +1,12 @@
+module "gateway" {
+  source   = "../../../../../ska-ser-orchestration/openstack-instance"
+  defaults = var.defaults
+  providers = {
+    openstack = openstack
+  }
+
+  configuration = {
+    name         = join("-", [var.datacentre, var.environment, var.service])
+    image        = "ubuntu-jammy-22.04"
+  }
+}
---
+++ datacentres/stfc-techops/production/orchestration/gateway/variables.tf
@@ -0,0 +1,35 @@
+variable "datacentre" {
+  type        = string
+  description = "The datacentre name"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment name"
+}
+
+variable "service" {
+  type        = string
+  description = "The orchestration service name"
+}
+
+variable "openstack" {
+  type = object({
+    cloud      = string
+    project_id = string
+  })
+}
+
+variable "defaults" {
+  type = object({
+    availability_zone   = string
+    flavor              = string
+    image               = string
+    keypair             = string
+    network             = string
+    jump_host           = optional(string)
+    floating_ip_network = optional(string)
+    vpn_cidr_blocks     = optional(list(string))
+  })
+  description = "Set of default values used when creating OpenStack instances"
+}
